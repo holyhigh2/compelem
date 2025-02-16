4968a655d4a3291b0e05eff3f77e19322d64f557
holyhigh2 <ylem@foxmail.com>
2025-02-16 21:44:20 +0800

    v 0.3.4-b1

--- src/directives/Show.ts
+++ src/directives/Show.ts
@@ -2,24 +2,26 @@ import { isUndefined } from "myfx";
 import { Directive } from "../directive/Directive";
 import { EnterPoint, EnterPointType, directive } from "../directive/index";
 import { DirectiveUpdateTag } from "../types";
-
+type Cbk = (node: Element, isVisible: boolean) => void
 /**
  * display的快捷指令
  * 如果
  * @param isvisible 是否显示
  */
 class Show extends Directive {
-  created(point: EnterPoint, isVisible: boolean): void {
+  created(point: EnterPoint, isVisible: boolean, cbk?: Cbk): void {
     let el = point.startNode as HTMLElement;
     if (isUndefined(this.display)) {
       this.display = el.style.display;
     }
+    let fn = cbk
     this.renderComponent.nextTick(() => {
       el.style.display = isVisible ? this.display! : 'none';
+      if (fn) fn(el, isVisible)
     })
   }
   update(point: EnterPoint, newArgs: any[], oldArgs: any[]): DirectiveUpdateTag {
-    this.created(point, newArgs[0])
+    this.created(point, newArgs[0], newArgs[1])
     return DirectiveUpdateTag.NONE
   }
   display: string | undefined = undefined;
@@ -31,7 +33,7 @@ class Show extends Directive {
     super();
     this.point = point
   }
-  render(isVisible: boolean) {
+  render(isVisible: boolean, cbk?: Cbk) {
 
   }
 }
